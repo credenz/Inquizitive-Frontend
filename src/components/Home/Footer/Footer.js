3a7bda2d8bd2ff6039c12d054ff375a3ad005da3
khaziashwaq <65824390+khaziashwaq@users.noreply.github.com>
2021-06-25 14:34:44 +0530

    Update Footer.js

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
     <div>
       <footer className="foot">
         © PICT IEEE Student Branch | Developed by PISB Web Team
       </footer>
+      <button style={{cursor : 'Pointer'}} onClick={() => setIsOpen(!isOpen)}>Ourteam</button>
+      <Modal scrollable={true} className="modal-dialog" isOpen={isOpen} toggle={() => setIsOpen(!isOpen)}>
+        <ModalHeader  className="modal-head" toggle={() => setIsOpen(!isOpen)}>
+
+        </ModalHeader>
+        <ModalBody >
+          <Ourteam/>
+        </ModalBody>
+      </Modal>
     </div>
   );
 };
